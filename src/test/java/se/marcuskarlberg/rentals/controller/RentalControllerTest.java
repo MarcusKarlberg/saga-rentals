982cfd4e5a2cba229c78617361950e976660e4e0
markar7 <marcus.karlberg@knowit.se>
2025-08-06 16:46:38 +0200

    added missing controller test

--- src/test/java/se/marcuskarlberg/rentals/controller/RentalControllerTest.java
+++ src/test/java/se/marcuskarlberg/rentals/controller/RentalControllerTest.java
@@ -1,14 +1,24 @@
 package se.marcuskarlberg.rentals.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import se.marcuskarlberg.rentals.model.RentalDTO;
 import se.marcuskarlberg.rentals.service.RentalService;
 
 import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RentalController.class)
 public class RentalControllerTest {
@@ -16,16 +26,25 @@ public class RentalControllerTest {
   @Autowired
   private MockMvc mockMvc;
 
-  @Mock
+  @MockBean
   private RentalService rentalService;
 
   @Test
   void createRentalRequestTest() throws Exception {
     RentalDTO rentalDTO = RentalDTO.builder()
+      .id(UUID.randomUUID().toString())
       .articleId(UUID.randomUUID().toString())
       .itemName("Iphone")
       .price(99.0)
       .quantity(1)
       .build();
+
+    when(rentalService.createRentalRequest(any(RentalDTO.class))).thenReturn(rentalDTO);
+
+    mockMvc.perform(post("/rentals")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(new ObjectMapper().writeValueAsString(rentalDTO)))
+      .andExpect(status().isCreated())
+      .andExpect(content().json(new ObjectMapper().writeValueAsString(rentalDTO)));
   }
 }
